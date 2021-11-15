3984c6c6ebc80df236b1d54015623c8fdbf43a01
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-11-15 12:06:44 -0300

    Payment, one-to-one association

--- src/main/java/com/javacourse/webserviceproject/config/TestConfig.java
+++ src/main/java/com/javacourse/webserviceproject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.javacourse.webserviceproject.entities.Category;
 import com.javacourse.webserviceproject.entities.Order;
 import com.javacourse.webserviceproject.entities.OrderItem;
+import com.javacourse.webserviceproject.entities.Payment;
 import com.javacourse.webserviceproject.entities.Product;
 import com.javacourse.webserviceproject.entities.User;
 import com.javacourse.webserviceproject.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
